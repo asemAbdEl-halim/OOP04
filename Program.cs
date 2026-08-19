690156efdbfdb793a502a6146ec41db9a2c33a19
asemAbdEl-halim <asemabdelhalim78@gmail.com>
2026-08-19 12:54:23 +0300

    Question01_Part01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+
+            #region Question01
+
+            /*
+             * a) a)  What is Abstraction in Object-Oriented Programming?
+             * ==> Answer <==
+             * ==> Abstraction means hiding unnecessary details and showing only the important parts of an object.
+             * 
+             * b) Why is abstraction considered one of the four pillars of OOP?
+             * => Answer <==
+             * ==> Abstraction is one of the four pillars of OOP because it makes the code simpler, easier to understand, and easier to use by hiding complex implementation details.
+             * **/
+
+            #endregion
+
+            #endregion
         }
     }
 }
